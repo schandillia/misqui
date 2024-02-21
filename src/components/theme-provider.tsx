03e766e0ad8ff5f31658ecd99c108a6339e6155d
schandillia <amit@schandillia.com>
2024-02-21 14:53:21 +0530

    Theming setup

---
+++ src/components/theme-provider.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+"use client"
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+
+// eslint-disable-next-line import/prefer-default-export
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
