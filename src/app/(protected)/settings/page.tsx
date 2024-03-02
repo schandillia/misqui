3f6a408dca673ec45dc715bfcd097020325cc08a
schandillia <amit@schandillia.com>
2024-03-02 09:33:25 +0530

    Delete account button

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -3,6 +3,7 @@
 import { Input } from "@/components/ui/input"
 import useCurrentUser from "@/hooks/use-current-user"
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
 
 function Page() {
 	const user = useCurrentUser()
@@ -11,18 +12,18 @@ function Page() {
 		<main className="flex flex-col justify-between px-5">
 			<Table>
 				<TableBody>
-					<TableRow>
+					<TableRow className="hover:bg-transparent hover:text-inherit">
 						<TableCell className="font-medium">Username</TableCell>
 						<TableCell>
 							<Input
 								disabled
 								type="text"
 								defaultValue="someone123"
-								className="border-0 shadow-none focus:ring-0"
+								className="border-0 shadow-none focus:ring-0 h-full"
 							/>
 						</TableCell>
 					</TableRow>
-					<TableRow>
+					<TableRow className="hover:bg-transparent hover:text-inherit">
 						<TableCell className="font-medium">
 							Primary email
 						</TableCell>
@@ -31,11 +32,11 @@ function Page() {
 								disabled
 								type="email"
 								defaultValue={user?.email || ""}
-								className="border-0 shadow-none focus:ring-0"
+								className="border-0 shadow-none focus:ring-0 h-full"
 							/>
 						</TableCell>
 					</TableRow>
-					<TableRow>
+					<TableRow className="hover:bg-transparent hover:text-inherit">
 						<TableCell className="font-medium">
 							Secondary email
 						</TableCell>
@@ -44,19 +45,29 @@ function Page() {
 								disabled
 								type="email"
 								defaultValue="some@secondemail.com"
-								className="border-0 shadow-none focus:ring-0"
+								className="border-0 shadow-none focus:ring-0 h-full"
 							/>
 						</TableCell>
 					</TableRow>
-					<TableRow>
+					<TableRow className="hover:bg-transparent hover:text-inherit">
 						<TableCell className="font-medium">Country</TableCell>
 						<TableCell>
 							<Input
 								disabled
 								type="text"
 								defaultValue="India"
-								className="border-0 shadow-none focus:ring-0"
+								className="border-0 shadow-none focus:ring-0 h-full"
 							/>
+						</TableCell>
+					</TableRow>
+					<TableRow className="hover:bg-transparent hover:text-inherit">
+						<TableCell>
+							<Button
+								variant="link"
+								className="text-destructive dark:text-red-500 hover:no-underline pl-0"
+							>
+								Delete Account
+							</Button>
 						</TableCell>
 					</TableRow>
 				</TableBody>
